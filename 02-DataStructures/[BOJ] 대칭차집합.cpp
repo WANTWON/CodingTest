a2eb9982642d8f3f94a00bc24f7dc09063ed6adf
HyeYeon-Won <106663427+WANTWON@users.noreply.github.com>
2023-05-24 01:01:29 +0900

    Create [BOJ] 대칭차집합 / 실버4 / 15분
    https://www.acmicpc.net/problem/1269

---
+++ 02-DataStructures/[BOJ] 대칭차집합.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	map<int, int> set;
+
+	int num;
+	int cnt=0;
+
+	for (int i = 0; i < N+M; i++)
+	{
+		cin >> num;
+		set[num]++;
+
+		if (set[num] > 1)
+			cnt++;
+	}
+
+	cout << N+M-cnt*2 << "\n";
+
+
+	return 0;
+}
