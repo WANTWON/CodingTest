02a4cad21a473b541de3d28fc145f115880054be
HyeYeon-Won <106663427+WANTWON@users.noreply.github.com>
2023-05-28 01:45:56 +0900

    [BOJ] 알고리즘 수업 - 너비우선탐색 1 / 실버2 / 10분
    https://www.acmicpc.net/problem/24444

---
+++ 04-DFS&BFS/[BOJ] 알고리즘 수업 - 너비우선탐색 1.cpp
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<vector<int>> Graph;
+vector<int> isVisited;
+int step = 0;
+
+void dfs(int First, int level)
+{
+	if (isVisited[First] > 0)
+		return;
+
+	isVisited[First] = level;
+
+	sort(Graph[First].begin(), Graph[First].end());
+
+	for (auto iter : Graph[First])
+	{
+		dfs(iter, level + 1);
+	}
+}
+
+void bfs(int First)
+{
+	queue<int> q;
+	q.push(First);
+
+	step++;
+	isVisited[First] = step;
+
+	while (!q.empty())
+	{
+		int Inq = q.front();
+
+		q.pop();
+		for (auto iter : Graph[Inq])
+		{
+			if (isVisited[iter] == 0)
+			{
+				step++;
+				isVisited[iter] = step;
+				q.push(iter);
+			}
+		}
+	}
+
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m, First;
+	cin >> n >> m >> First;
+
+	Graph.resize(n + 1);
+	isVisited.resize(n + 1);
+
+	for (int i = 0; i < m; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		Graph[a].push_back(b);
+		Graph[b].push_back(a);
+	}
+	for (int i = 1; i <= n; i++)
+		sort(Graph[i].begin(), Graph[i].end());
+
+	bfs(First);
+
+	for (int i = 1; i <= n; i++)
+		cout << isVisited[i] << "\n";
+
+	return 0;
+}
