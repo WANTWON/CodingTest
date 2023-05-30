02f9452dd3376b57ac935c4ebe235c66e4937675
HyeYeon-Won <106663427+WANTWON@users.noreply.github.com>
2023-05-30 16:12:01 +0900

    [BOJ] N과 M (4) / 실버3 / 10분
    https://www.acmicpc.net/problem/15652

---
+++ 05-BackTracking/[BOJ] N과 M (4).cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <string.h>
+#include <string>
+#include <vector>
+
+#define MAX 8
+
+using namespace std;
+
+int n, m;
+int arr[MAX];
+bool visited[MAX];
+
+
+void BT(int level)
+{
+	if (level == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (level == 0 || i >= arr[level - 1])
+		{
+			arr[level] = i;
+			BT(level + 1);
+		}
+
+	}
+
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> n >> m;
+	BT(0);
+
+	return 0;
+}
