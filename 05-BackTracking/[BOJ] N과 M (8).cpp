c21b2abb60ed6f415f8d8d519dc4e0bd1149bddb
HyeYeon-Won <106663427+WANTWON@users.noreply.github.com>
2023-06-06 01:02:40 +0900

    [BOJ] N과 M (8) / 실버3 / 5분
    https://www.acmicpc.net/problem/15657

---
+++ 05-BackTracking/[BOJ] N과 M (8).cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <vector>
+
+
+using namespace std;
+
+vector<int> arr;
+int visited[9];
+int N, M;
+int ans[9];
+
+void BT(int k)
+{
+	if (k == M)
+	{
+		for (int i = 0; i < M; i++)
+			cout << ans[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int j = 0; j < N; j++)
+	{
+		if (k != 0 && ans[k - 1] > arr[j])
+			continue;
+
+			ans[k] = arr[j];
+			visited[j] = 1;
+			BT(k + 1);
+			visited[j] = 0;
+		
+	}
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	
+	cin >> N >> M;
+
+	
+	for (int j = 0; j < N; j++)
+	{
+		int Temp;
+		cin >> Temp;
+		arr.push_back(Temp);
+	}
+	sort(arr.begin(), arr.end());
+
+	BT(0);
+	
+
+}
