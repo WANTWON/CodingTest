3d38faf9b2a18fc34674aa83c01dbda2884641c8
HyeYeon-Won <106663427+WANTWON@users.noreply.github.com>
2023-10-14 15:23:36 +0900

    [BOJ] 최대 힙 /실버2 / 1분
    https://www.acmicpc.net/problem/11279

---
+++ 12-Priority Queue/[BOJ] 최대 힙.cpp
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <queue>
+
+using namespace std;
+
+int N;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> N;
+	priority_queue<int> PQ;
+	//priority_queue<int, vector<int>, greater<int>> PQ;
+
+	int tmp;
+
+	while (N--)
+	{
+		cin >> tmp;
+
+		if (tmp == 0)
+		{
+			if (PQ.empty()) cout << 0 << "\n";
+			else
+			{
+				cout << PQ.top() << "\n";
+				PQ.pop();
+			}
+		}
+		else
+		{
+			PQ.push(tmp);
+		}
+	}
+
+
+	return 0;
+}
